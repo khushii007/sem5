5e61433ec3582b0d33004ecb5793fc2c163e3328
Khushi Israni <113411572+khushii007@users.noreply.github.com>
2023-08-09 01:08:13 +0530

    lab1 done

---
+++ OSL/lab1/q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int integerNo = 13;
+    float floatNo = 2.234;
+    char character = 'Z';
+    char string[] = "Hello, world!";
+    unsigned int unsignedInteger = 123456;
+    printf("Integer: %d\n", integerNo);
+    printf("Float: %f\n", floatNo);
+    printf("Character: %c\n", character);
+    printf("String: %s\n", string);
+    printf("Unsigned Integer: %u\n", unsignedInteger);
+    return 0;
+}
